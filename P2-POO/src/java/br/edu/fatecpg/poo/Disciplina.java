7d7f29ed2f10fbc1a03c4feb10eb4a18cf11d370
gui947 <64759354+gui947@users.noreply.github.com>
2020-11-30 01:22:06 -0300

    Disciplina

--- P2-POO/src/java/br/edu/fatecpg/poo/Disciplina.java
+++ P2-POO/src/java/br/edu/fatecpg/poo/Disciplina.java
@@ -1,77 +1,122 @@
 package br.edu.fatecpg.poo;
 
-public class Disciplina {
+import java.sql.*;
+import java.util.ArrayList;
+
+    public class Disciplina{
+        private long rowid;
+        private String nome;
+        private String ementa;
+        private Integer ciclo;
+        private Double nota;
+        
+        public static ArrayList<Disciplina>getList(){
+           
+            ArrayList<Disciplina>list = new ArrayList<>();
+            Connection con = null;
+            Statement stmt = null;
+            ResultSet rs = null;
+            
+            try{
+                con = DBListener.getConnection();
+                stmt = con.createStatement();
+                rs = stmt.executeQuery("Select rowid,*FROM disciplina");
+                while(rs.next()){
+                    list.add(new Disciplina(rs.getLong("rowid"),rs.getString("nome"),rs.getString("ementa"),rs.getInt("ciclo"),rs.getDouble("nota")));
+                }
+            }catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+            return list;
+        }
+        
+        public static void insert(String nome, String ementa, int ciclo, Double nota) throws Exception{
+            Connection con = null;
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            
+            try{
+                con = DBListener.getConnection();
+                stmt = con.prepareStatement("INSERT INTO disciplina VALUES(?,?,?,?)");
+                stmt.setString(1,nome);
+                stmt.setString(2, ementa);
+                stmt.setInt(3, ciclo);
+                stmt.setDouble(4, nota);
+                stmt.execute();
+            }catch(Exception ex){
+                System.out.println(ex);
+            }
+        }
+        
+        public static void delete(long rowid){
+            Connection con = null;
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            try{
+                con = DBListener.getConnection();
+                stmt = con.prepareStatement("DELETE FROM disciplina WHERE rowid=?");
+                stmt.setLong(1,rowid);
+                stmt.execute();
+            }catch(Exception ex){
+                System.out.println(ex);
+            }
+        }
+        
     
-    private String na;
-    private long ra;
-    private int qtd;
-    private String name;
-    private String ementa;
-    private int ciclo;
-    private double nota;
-    
-    public Disciplina(String na, long ra, int qtd, String name, String ementa, int ciclo, double nota){
-        this.na = na;
-        this.ra = ra;
-        this.qtd = qtd;
-        this.name = name;
+    public Disciplina(long rowid,String nome, String ementa, int ciclo, double nota) {
+        this.rowid = rowid;
+        this.nome = nome;
         this.ementa = ementa;
         this.ciclo = ciclo;
         this.nota = nota;
     }
-    public String getNameAluno(){
-        return na;
+    
+    public long getRowid() {
+        return rowid;
     }
-    public void setNameAluno(String na){
-        this.na = na;
+
+    public void setRowid(long rowid) {
+        this.rowid = rowid;
     }
-    public long getRa(){
-        return ra;
+    
+    public String getNome() {
+        return nome;
     }
-    public void setRa(int ra){
-        this.ra = ra;
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
-    public int getQtd(){
-        return qtd;
-    }
-    public void setQtd(int qtd){
-        this.qtd = qtd;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setNome(String name){
-        this.name = name; 
-    }
-    public String getEmenta(){
+
+    public String getEmenta() {
         return ementa;
     }
-    public void setEmenta(String ementa){
+
+    public void setEmenta(String ementa) {
         this.ementa = ementa;
     }
-    public int getCiclo(){
-        if(ciclo<1)
-            this.ciclo = 1;
-        else if (ciclo>6)
-            this.ciclo = 6;
-        else 
-            this.ciclo = ciclo;
+
+    public int getCiclo() {
         return ciclo;
     }
-    public void setCiclo(int ciclo){
+
+    public void setCiclo(int ciclo) {
         this.ciclo = ciclo;
     }
-    public double getNota(){
-        if(nota<0)
-            this.nota = 0;
-        else if (nota>10)
-            this.nota = 10;
-        else 
-            this.nota = nota;
+
+    public double getNota() {
         return nota;
     }
-    public void setNota(double nota){
+
+    public void setNota(double nota) {
         this.nota = nota;
     }
+    
+    public static String getCreateStatment(){
+        return "CREATE TABLE IF NOT EXISTS disciplina("
+               +"nome VARCHAR(50) UNIQUE NOT NULL,"
+               +"ementa VARCHAR(200) NOT NULL,"
+               +"ciclo INT NOT NULL,"
+               +"nota INT NOT NULL,"
+               +")";
+    }
 }
-
